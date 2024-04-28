1fda3336dfe0ef79234cfd1a7082e6c500480ed2
iRash77ka <irash77ka@gmail.com>
2024-04-28 18:37:33 +0300

    add file

---
+++ homework3.py
@@ -0,0 +1,16 @@
+my_string = input("Введите ваше имя ")
+print("Привет", my_string)
+
+
+my_string = "Привет Urban"                                                              #Длина строки
+print(len(my_string))
+
+
+my_string = "Всем привет"                                                               #Вывод первого символа
+print(my_string[0])
+my_string = "Всем привет"                                                               #Вывод последнего символа
+print(my_string[-1])
+
+print("удаляем все пробелы в строке" .replace(" ", ""))                     #удаляем все пробелы в строке
+print("urban" .upper())                                                                 #Верхний регистр
+print("URBAN" .lower())                                                                 #Нижний регистр
