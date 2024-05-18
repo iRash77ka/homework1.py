dea492d1cd55a914d6e6d4c334be9e5f018a75f4
iRash77ka <irash77ka@gmail.com>
2024-05-18 19:55:53 +0300

    add file

---
+++ homework5.py
@@ -0,0 +1,18 @@
+#food = ["apple", "coconut", "banana", "orange", "pear"]
+#print(food)
+
+#res = [food[i] for i in (0, -1)]
+#print(str(res))
+
+#print(food[0:5])
+#print(food[2:6])
+
+#food[2] = "peach"
+#print(food)
+
+
+#my_dict = {'apple':'яблоко', 'coconut':'кокос', 'banana':'банан', 'orange':'апельсин', 'pear':'персик'}
+#print(my_dict)
+#print(my_dict['banana'])
+#my_dict['orange'] ='ананас'
+#print(my_dict)
